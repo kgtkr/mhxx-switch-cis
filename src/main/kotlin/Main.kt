2a3e70a70d1b3b867707ca5f78c1d1c6ca0f5841
kgtkr <kgtkr.jp@gmail.com>
2017-09-02 13:08:02 +0900

    cオプションで余計な画像も見つけるように 余計な画像削除 cオプションの"value"を"value0","value1","value2"にした cオプションに使うマークダウンの空白を"(なし)"に変更

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -145,19 +145,31 @@ fun readImages(dir:String):List<Rows>{
 fun checkCmd(){
     fun check(name:String){
         println("【${name}】")
-        FileUtils.readFileToString(File("./images-${name}.md"),"utf8")
+        //必要なファイル
+        val files=FileUtils.readFileToString(File("./images-${name}.md"),"utf8")
                 .split("\n")
                 .filter{s->s.indexOf("* [x] ")==0}
                 .map{s->s.substring(6)}
+                .map{s->if(s=="(なし)") "" else s}
                 .map{s->File("./images/${name}/${s}.jpg")}
-                .filter { file-> !file.exists()}
-                .forEach{file->println(file.name)}
+
+        //不足
+        files.filter { file-> !file.exists()}
+            .forEach{file->println("不足:"+file.name)}
+        //余計
+        File("./images/${name}/")
+                .listFiles()
+                .filter{f->files.indexOf(f)==-1}
+                .forEach{file->println("余計:"+file.name)}
+
         println()
     }
 
     check("goseki")
     check("skill")
-    check("value")
+    check("value0")
+    check("value1")
+    check("value2")
     check("slot")
 }
 
